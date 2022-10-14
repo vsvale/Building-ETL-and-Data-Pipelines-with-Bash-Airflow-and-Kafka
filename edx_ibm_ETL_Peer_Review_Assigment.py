80b677b02ac13bdf748009d295db174dd0f5b28c
Vinicius da Silva Vale <viniciusdvale@gmail.com>
2022-10-13 23:22:23 -0300

    fix import DAG

--- edx_ibm_ETL_Peer_Review_Assigment.py
+++ edx_ibm_ETL_Peer_Review_Assigment.py
@@ -2,7 +2,7 @@
 
 from datetime import timedelta
 # The DAG object; we'll need this to instantiate a DAG
-from airflow.decorators import dag
+from airflow import DAG
 # Operators; we need this to write tasks!
 from airflow.operators.bash_operator import BashOperator
 # This makes scheduling easy
